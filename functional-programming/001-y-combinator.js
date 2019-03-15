74ee01c1e00d4dfd3caaac448012316bc329d03b
王子龙 <wangzilong03@meituan.com>
2019-03-15 18:43:28 +0800

    featurea: add fp dir

---
+++ functional-programming/001-y-combinator.js
@@ -0,0 +1,13 @@
+
+const param = (f) =>
+  (n) =>
+    n === 0 || n === 1 ? 1 : f(n - 1) + f(n - 2);
+
+function Y(func) {
+  return (n) => 
+}
+
+const fibonacci = Y(param);
+
+let res1 = fibonacci(3);
+console.log(res1);
